6cf7813c8c53516a8a00fd359bf9c7da911dcb34
PBBassily <paulaboules@pharos-mac-sever.local>
2018-09-17 14:40:00 +0200

    tableview for viewing/inserting/deleting documents

---
+++ EmojiDrawer/EmojiDocumentTableViewController.swift
@@ -0,0 +1,91 @@
+//
+//  EmojiDocumentTableViewController.swift
+//  EmojiDrawer
+//
+//  Created by Paula Boules on 9/17/18.
+//  Copyright © 2018 Paula Boules. All rights reserved.
+//
+
+import UIKit
+
+class EmojiDocumentTableViewController: UITableViewController {
+
+    var documents =  ["one", "two","three","four","five"]
+    
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return documents.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DocumentChooser", for: indexPath)
+
+        cell.textLabel?.text = self.documents[indexPath.row]
+
+        return cell
+    }
+    
+    @IBAction func addEmojiDocument(_ sender: UIBarButtonItem) {
+        documents += ["document".madeUnique(withRespectTo: documents)]
+        tableView.reloadData()
+        
+    }
+    
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            
+            documents.remove(at: indexPath.row)
+            
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            
+           
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
